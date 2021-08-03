a06519b93bec34f2e78653afa20587b69014d90c
VincentG2021 <vincentgade@gmail.com>
2021-08-03 16:57:11 +0200

    first version immo-eliza-app

--- immo-eliza-app/src/App.js
+++ immo-eliza-app/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import logo from './mouettenobg.png';
 import './App.css';
+import Formulario from './Components/Formulario';
+
+
 
 function App() {
+
+// FETCH avec immo-Eliza API
+  // fetch('https://salty-mesa-39646.herokuapp.com/predict', {
+  //       method: 'POST', // or 'PUT'
+  //       mode: 'cors',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({ "data": {"zip-code": 4000, "area": 100, "property-type": 'APARTMENT', "rooms-number": 2, "property-subtype": 'VILLA'}}),
+
+  //     })
+  //     .then(response => response.json())
+
+  //     .then(data => {
+  //       console.log('Success:', data);
+  //       // console.log(response)
+  //     })
+  //     .catch((error) => {
+  //         console.error(error)
+  //     });
+
   return (
     <div className="App">
       <header className="App-header">
+      <div className='spiralContainer'>
+        <div className='spiral'>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </div>
+      </div>
       </header>
+    <>
+      <Formulario />
+    </>
     </div>
   );
 }
